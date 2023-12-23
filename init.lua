59dba8eec7292e1127093eac20a38d0c159b4a27
Lewis Sneddon <lewis@lewiscodes.io>
2023-12-23 16:09:11 +0000

    nvim tree

--- init.lua
+++ init.lua
@@ -375,7 +375,7 @@ vim.defer_fn(function()
   require('nvim-treesitter.configs').setup {
     -- Add languages to be installed here that you want installed for treesitter
     ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim',
-      'bash', 'help' },
+      'bash' },
 
     -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
     auto_install = false,
--- lua/lewis/plugins/nvim-tree.lua
+++ lua/lewis/plugins/nvim-tree.lua
@@ -9,8 +9,12 @@ return {
     -- disable netrw at the very start of your init.lua
     vim.g.loaded_netrw = 1
     vim.g.loaded_netrwPlugin = 1
+
+    require('nvim-web-devicons').setup {}
+
     vim.api.nvim_set_keymap('n', '<leader>tt', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
     vim.api.nvim_set_keymap('n', '<leader>tf', ':NvimTreeFocus<CR>', { noremap = true, silent = true })
+
     require("nvim-tree").setup {}
   end,
 }
