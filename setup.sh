dabb6d51cc0052e0889369a104e640b551036dda
Lewis Sneddon <lewis@lewiscodes.io>
2023-12-23 15:11:11 +0000

    set config path if on windows

--- setup.sh
+++ setup.sh
@@ -2,6 +2,11 @@
 
 nvim_config="${XDG_CONFIG_HOME:-$HOME/.config}/nvim"
 
+# set Neovim config path for Windows
+if [ "$(expr substr $(uname -s) 1 5)" = "MINGW" ]; then
+    nvim_config="$USERPROFILE/AppData/Local/nvim"
+fi
+
 rm -rf "$nvim_config"
 
 mkdir -p "$nvim_config"
